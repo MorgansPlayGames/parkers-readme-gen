f2c16b4896e0225da6e652873f0d3b325d4688d7
Parker Morgan <jpmaster13@msn.com>
2020-11-12 23:46:50 -0700

    Added questions and console.log result with inquirer

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+
+const inquirer = require('inquirer');
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Project Description: "
+    },
+    {
+        type: 'input',
+        name: 'installInstructions',
+        message: "How do I install: "
+    },
+    {
+        type: 'input',
+        name: 'usageInformation',
+        message: 'How do we use this program?'
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'Contribution guidelines: '
+    },
+    {
+        type: 'list',
+        name: 'licence',
+        message: 'what licence do you want? ',
+        choices: ['none', 'MIT']
+    },
+    {
+        type: 'input',
+        name: 'GitHubUsername',
+        message: 'GitHub username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email Address: '
+    }
 ];
 
 // function to write README file
@@ -9,7 +51,16 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    
+    inquirer
+    .prompt(
+        /* Pass your questions in here */
+        questions
+    )
+    .then(answers => {
+        // Use user feedback for... whatever!!
+        console.log(answers);
+    });
 }
 
 // function call to initialize program
